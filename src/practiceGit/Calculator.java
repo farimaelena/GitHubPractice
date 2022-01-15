70003b90b9701360ada5997ac14b62f98f572e49
elenafarima <farimaelena@yahoo.com>
2022-01-15 10:28:46 -0500

    created divide method

--- src/practiceGit/Calculator.java
+++ src/practiceGit/Calculator.java
@@ -4,13 +4,19 @@ public class Calculator {
     public static void main(String[] args) {
 
     }
-public static int add(int num1, int num2){
+
+    public static int add(int num1, int num2) {
         int result = num1 + num2;
         return result;
-}
-    public static int subtract(int num1,int num2){
+    }
+
+    public static int subtract(int num1, int num2) {
         int result = num1 - num2;
         return result;
     }
 
+    public static int divide(int num1, int num2) {
+        int result = num1 / num2;
+        return result;
+    }
 }

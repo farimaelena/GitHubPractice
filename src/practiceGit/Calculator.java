550c140d1726b5e25e7f7ef921ecaeaa9f8c5fab
elenafarima <farimaelena@yahoo.com>
2022-01-15 10:28:12 -0500

    created subtract method

--- src/practiceGit/Calculator.java
+++ src/practiceGit/Calculator.java
@@ -4,5 +4,13 @@ public class Calculator {
     public static void main(String[] args) {
 
     }
+public static int add(int num1, int num2){
+        int result = num1 + num2;
+        return result;
+}
+    public static int subtract(int num1,int num2){
+        int result = num1 - num2;
+        return result;
+    }
 
 }

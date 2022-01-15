d1a9d6e6ecc2807dd2e2cf5d959ff537649b38c2
elenafarima <farimaelena@yahoo.com>
2022-01-15 10:30:17 -0500

    created multiply method

--- src/practiceGit/Calculator.java
+++ src/practiceGit/Calculator.java
@@ -19,4 +19,9 @@ public class Calculator {
         int result = num1 / num2;
         return result;
     }
+
+    public static int multiply(int num1, int num2) {
+        int result = num1 * num2;
+        return result;
+    }
 }
